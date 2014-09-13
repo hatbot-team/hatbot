7c7d063e396d8f5d24f60d337e78da2d24220362
pershik <k.pershik@gmail.com>
2014-09-13 18:47:15 +0400

    Some bugs fixed

--- lang_utils/antonyms/antonyms.py
+++ lang_utils/antonyms/antonyms.py
@@ -4,4 +4,7 @@ from . import _init_antonyms
 
 
 def get_antonyms(word):
-    return _init_antonyms.antonyms[word]
+    if word in _init_antonyms.antonyms:
+        return _init_antonyms.antonyms[word]
+    else:
+        return []
