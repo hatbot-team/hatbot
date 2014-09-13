d1bcc5c23f361452f262c4712fd4fab6e4fa1045
pershik <k.pershik@gmail.com>
2014-09-13 19:21:31 +0400

    Added doctests to get_antonyms

--- lang_utils/antonyms/antonyms.py
+++ lang_utils/antonyms/antonyms.py
@@ -1,9 +1,26 @@
+# coding:utf8
+
 __author__ = 'pershik'
 
-from . import _init_antonyms
+try:
+    from . import _init_antonyms
+except SystemError:
+    import _init_antonyms
 
 
 def get_antonyms(word):
+
+    """
+    Get antonyms list for the given word
+
+    >>> get_antonyms('свет')
+    ['тьма', 'мрак', 'темнота', 'тень']
+    >>> get_antonyms('диван')
+    []
+
+    :param word: russian word in the initial form
+    :return: list of all its antonyms
+    """
     if word in _init_antonyms.antonyms:
         return _init_antonyms.antonyms[word]
     else:
