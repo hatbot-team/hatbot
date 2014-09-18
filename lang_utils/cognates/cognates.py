2a2a07af0dddabe375e13c6b19d1c258f8cfd1fe
Feodor Alexeev <feodor.alexeev@gmail.com>
2014-09-18 15:02:16 +0400

    Added initial forms check to cognates module

--- lang_utils/cognates/cognates.py
+++ lang_utils/cognates/cognates.py
@@ -1,5 +1,7 @@
 __author__ = 'moskupols'
 
+from lang_utils.morphology import get_initial_forms
+
 def are_cognates(a, b):
     """
     Checks two words for having same root.
@@ -8,9 +10,11 @@ def are_cognates(a, b):
 
     >>> are_cognates('мама', 'рама')
     False
+    >>> are_cognates('мама', 'маме')
+    True
 
     :param a: string containing russian word
     :param b: same as a
     :return: True if a and b are cognates, False otherwise.
     """
-    return False
+    return not set(get_initial_forms(a)).isdisjoint(get_initial_forms(b))
