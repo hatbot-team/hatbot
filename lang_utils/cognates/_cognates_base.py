4fc54cbf3a46a2f95364047c3734945a8b95dea1
Daria Kolodzey <kolodaria@google.com>
2014-09-22 12:38:31 +0400

    "added dictionary check to are_cognates"

---
+++ lang_utils/cognates/_cognates_base.py
@@ -0,0 +1,31 @@
+__author__ = 'Oktosha'
+
+import os
+from sys import stderr
+import codecs
+
+COGNATES_PATH = \
+    os.path.dirname(os.path.abspath(__file__)) + '/cognates.txt'
+
+
+def init_cognates():
+    try:
+        cognates_file = codecs.open(COGNATES_PATH, "r", "utf-8")
+    except:
+        stderr.write('Cognates dictionary doesn\'t exist\n')
+        return
+    global cognates
+    text = cognates_file.read()
+    articles = text.split('\n\n')
+    articles = [s.split('\n') for s in articles]
+    articles = [[s[0].lower()] + sorted(set(s[1:])) for s in articles]
+    for s in articles:
+        for word in s[1:]:
+            cognates[word] = []
+    for s in articles:
+        for word in s[1:]:
+            cognates[word].append(s[0])
+
+
+cognates = dict()
+init_cognates()
--- lang_utils/cognates/cognates.py
+++ lang_utils/cognates/cognates.py
@@ -1,20 +1,51 @@
-__author__ = 'moskupols'
+__author__ = 'Oktosha'
 
 from lang_utils.morphology import get_initial_forms
+
+try:
+    from . import _cognates_base
+except SystemError:
+    import _cognates_base
+
+def get_roots(word):
+    """
+    Returns list of root-words which are titles of dictionary articles containing the given word.
+    If the dictionary doesn't contain the given word the list containing word itself is returned.
+
+    :param word: russian word
+    :return: list of root-words
+    """
+    """
+    :param word:
+    :return:
+    """
+    if word in _cognates_base.cognates:
+        return _cognates_base.cognates[word]
+    else:
+        return [word]
 
 def are_cognates(a, b):
     """
     Checks two words for having same root.
 
-    Being a stub, currently it always returns False.
-
     >>> are_cognates('мама', 'рама')
     False
     >>> are_cognates('мама', 'маме')
+    True
+    >>> are_cognates('мама', 'маменька')
     True
 
     :param a: string containing russian word
     :param b: same as a
     :return: True if a and b are cognates, False otherwise.
     """
-    return not set(get_initial_forms(a)).isdisjoint(get_initial_forms(b))
+
+    a_roots = []
+    for word in get_initial_forms(a):
+        a_roots.extend(get_roots(word))
+
+    b_roots = []
+    for word in get_initial_forms(b):
+        b_roots.extend(get_roots(word))
+
+    return not set(a_roots).isdisjoint(b_roots)
