e08f1c82477b3c1deb0dcb9b57bcb80ecd4f792c
Feodor Alexeev <feodor.alexeev@gmail.com>
2014-09-25 16:54:01 +0400

    get_dicts.sh for convenient dictionaries download
    Had to upload some to my dropbox: it seems to be
    impossible to wget files from ydisk.

---
+++ get_dicts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -nv -O lang_utils/antonyms/antonyms.txt https://www.dropbox.com/s/xv3fxunlfdome3f/antonyms.txt
+wget -nv -O lang_utils/synonyms/synonyms.txt https://www.dropbox.com/s/c86vvdxua5plezc/synonyms.txt
+wget -nv -O lang_utils/cognates/cognates.txt https://www.dropbox.com/s/mkzgtclsev33ze5/cognates.txt
+wget -nv -O explanations/sources/collocations/collocations.txt https://www.dropbox.com/s/jso793ge8sptj2r/collocations.txt
+wget -nv -O explanations/sources/phraseological/phraseologism.txt https://www.dropbox.com/s/m45jngri823s983/phraseologism.txt
+
