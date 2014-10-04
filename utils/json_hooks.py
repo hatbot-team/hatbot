8ec5ddf9b8cb7c0e5cd0911b5aad23e96ef360d3
Feodor Alexeev <feodor.alexeev@gmail.com>
2014-10-04 06:52:23 +0400

    Hided JSON helper functions

--- utils/json_hooks.py
+++ utils/json_hooks.py
@@ -2,7 +2,9 @@ __author__ = 'moskupols'
 
 import json
 from collections import namedtuple
+
 from explanations import Explanation
+
 
 """
   A wrapper for standard json module that preserves *tuples* and some other classes.
@@ -12,11 +14,8 @@ from explanations import Explanation
 """
 
 
-def serializable(data):
+def _serializable(data):
     """ Borrowed from http://robotfantastic.org/serializing-python-data-to-json-some-edge-cases.html
-
-    :param data:
-    :return:
     """
     def is_namedtuple(obj):
         """Heuristic check if an object is a namedtuple."""
@@ -30,27 +29,28 @@ def serializable(data):
     if isinstance(data, Explanation):
         return {'py/Explanation': {
             'source': data.source_name,
-            'key': serializable(data.key)
+            'key': _serializable(data.key)
         }}
     if isinstance(data, list):
-        return [serializable(val) for val in data]
+        return [_serializable(val) for val in data]
     if is_namedtuple(data):
         return {'py/namedtuple': {
             "name":   type(data).__name__,
             "fields": list(data._fields),
-            "values": [serializable(getattr(data, f)) for f in data._fields]
+            "values": [_serializable(getattr(data, f)) for f in data._fields]
         }}
     if isinstance(data, dict):
         if all(isinstance(k, str) for k in data):
-            return {k: serializable(v) for k, v in data.items()}
+            return {k: _serializable(v) for k, v in data.items()}
         return {'py/dict':
-            [[serializable(k), serializable(v)] for k, v in data.items()]
+            [[_serializable(k), _serializable(v)] for k, v in data.items()]
         }
     if isinstance(data, tuple):
-        return {"py/tuple": [serializable(val) for val in data]}
-    raise TypeError("Type %s not data-serializable" % type(data))
+        return {"py/tuple": [_serializable(val) for val in data]}
+    raise TypeError("Type %s not data-_serializable" % type(data))
 
-def object_decode_hook(dct):
+
+def _object_decode_hook(dct):
     """
     Tries to convert the given dict to an _DECODE_HOOKED, checking that it was
     previously generated for using with JSON.
@@ -72,18 +72,18 @@ def object_decode_hook(dct):
 
 
 def load(fp, **kwargs):
-    kwargs['object_hook'] = object_decode_hook
+    kwargs['object_hook'] = _object_decode_hook
     return json.load(fp, **kwargs)
 
 
 def loads(s, **kwargs):
-    kwargs['object_hook'] = object_decode_hook
+    kwargs['object_hook'] = _object_decode_hook
     return json.loads(s, **kwargs)
 
 
 def dump(o, fp, **kwargs):
-    return json.dump(serializable(o), fp, **kwargs)
+    return json.dump(_serializable(o), fp, **kwargs)
 
 
 def dumps(o, **kwargs):
-    return json.dumps(serializable(o), **kwargs)
+    return json.dumps(_serializable(o), **kwargs)
