b4c07699db47deeadd95ed2fba2d326c83c270d8
Feodor Alexeev <feodor.alexeev@gmail.com>
2014-10-08 23:16:06 +0400

    Fixed error on calling hatbot.py without params

--- hatbot.py
+++ hatbot.py
@@ -24,22 +24,29 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
         description='''Hatbot utilities. See hatbot.py utility -h for info on utility params.''')
+    parser.set_defaults(func=None)
     subparsers = parser.add_subparsers()
 
     server_parser = subparsers.add_parser('server',
-                                          help='''Run server.
+                                          help='''Run server. Localhost (127.0.0.1:8080) by default.
              Note that the server restarts automatically every time some source file is changed.
              Even more, at 93.175.... it is checked every 10m and restarted if found not running.
         ''')
     server_parser.add_argument('--config',
-                               help='path to the server config file')
+                               help='''Path to cherrypy config file. 
+             See https://cherrypy.readthedocs.org/en/latest/basics.html#configuring
+             ''')
     server_parser.set_defaults(func=run_server)
 
-    play_parser = subparsers.add_parser('play', help='run statistics gathering utility')
+    play_parser = subparsers.add_parser('play', help='Run statistics gathering utility')
     play_parser.set_defaults(func=play)
 
-    stats_parser = subparsers.add_parser('stats', help='output statistics')
+    stats_parser = subparsers.add_parser('stats', help='Output statistics')
     stats_parser.set_defaults(func=stats)
 
     args = parser.parse_args()
-    args.func(args)
+    if args.func is None:
+        parser.print_usage()
+    else:
+        args.func(args)
+
