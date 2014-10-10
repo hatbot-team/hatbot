34f38260a2e6fec7ba581dd85f7a5c7f26a9f418
Maxim Rusak <mak-rusak@yandex.ru>
2014-10-10 05:03:54 +0400

    Add link to film_titles.txt to get_dicts.sh

--- get_dicts.sh
+++ get_dicts.sh
@@ -5,4 +5,4 @@ wget -nv -O explanations/sources/synonyms/synonyms.txt https://www.dropbox.com/s
 wget -nv -O lang_utils/cognates/cognates.txt https://www.dropbox.com/s/mkzgtclsev33ze5/cognates.txt
 wget -nv -O explanations/sources/collocations/collocations.txt https://www.dropbox.com/s/jso793ge8sptj2r/collocations.txt
 wget -nv -O explanations/sources/phraseological/phraseologism.txt https://www.dropbox.com/s/m45jngri823s983/phraseologism.txt
-
+wget -nv -O explanations/sources/film_titles/film_titles.txt https://dl.dropboxusercontent.com/u/42829530/film_titles.txt
