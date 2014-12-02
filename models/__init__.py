0251aceec1a4c049984e2077e5fbc76e1d2bbf61
Feodor Alexeev <feodor.alexeev@gmail.com>
2014-12-02 03:37:44 +0300

    Add some docs for models

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,33 @@
+"""
+This package contains database models and access functions, all relying on peewee Object Relational Mapper that
+appears to be both lightweight and expressive.
+
+Please note that you MUST call init_database and optionally create_nonexistent_tables prior to using any model.
+
+>>> import datetime
+>>> from models import init_database, create_nonexistent_tables, Feedback
+>>> init_database('sqlite:///:memory:')
+>>> create_nonexistent_tables()
+>>> ts1 = datetime.datetime(2014, 3, 1, 4, 1, 5, 926)
+>>> ts2 = datetime.datetime(1828, 2, 7, 1, 8, 2, 8)
+>>> good = Feedback(verdict='GOOD', timestamp=ts1, expl_key='some key')
+>>> good.save()
+1
+>>> good.expl_key
+'some key'
+>>> viol = Feedback.create(verdict='VIOLATES', timestamp=ts2, expl_key='another key')
+>>> viol.timestamp
+datetime.datetime(1828, 2, 7, 1, 8, 2, 8)
+>>> for f in Feedback.select().order_by(Feedback.verdict):
+...     print(f.verdict, f.timestamp, f.expl_key)
+GOOD 2014-03-01 04:01:05.000926 some key
+VIOLATES 1828-02-07 01:08:02.000008 another key
+>>> good.delete_instance()
+1
+>>> for entry in Feedback.raw('select count(*) as cnt from feedback'):
+...     print(entry.cnt)
+1
+"""
 
 __all__ = [
     'get_database',
@@ -6,11 +36,18 @@ __all__ = [
     'Feedback'
 ]
 
-from ._database import init_database, get_database
-from .feedback import Feedback
+try:
+    from ._database import init_database, get_database
+    from .feedback import Feedback
+except SystemError:
+    from _database import init_database, get_database
+    from feedback import Feedback
 
 
 def create_nonexistent_tables():
+    """
+    Creates all necessary tables that do not exist yet in the database.
+    """
     tables = (
         Feedback,
     )
--- models/_database.py
+++ models/_database.py
@@ -7,6 +7,15 @@ database_url = None
 
 
 def init_database(url: str='sqlite:///hatbot.sqlite'):
+    """
+    Initializes database connection.
+    See https://peewee.readthedocs.org/en/latest/peewee/database.html?highlight=url#connecting-using-a-database-url
+    for info on URL format.
+
+    Please note that psycopg2 package has to be installed if we want to use PostgreSQL engine.
+
+    :param url: str describing type and parameters of connection
+    """
     global database, database_url
     assert database_url is None, 'database should be initialized exactly once'
 
